2d81560357be4ae0fde5f1315f608658e947d1d1
Christian Smorra <smorra.christian@gmail.com>
2019-02-20 13:55:35 +0100

    refactor, add external method to class

--- operators/s3_to_mongo_import_operator.py
+++ operators/s3_to_mongo_import_operator.py
@@ -7,63 +7,6 @@ from mongo_plugin.hooks.mongo_hook import MongoHook
 import os
 
 
-def make_mongo_import_command(
-        uri,
-        collection,
-        file,
-        mode='upsert',
-        type='json',
-        query=None,
-        fields=None,
-        columns_have_types=False,
-        upsert_fields=None,
-        extra_params=[]
-):
-    """
-    :param query:
-    :param fields:
-    :return:
-    """
-    fields_param = ""
-    upsert_fields_param = ""
-    columns_have_types_param = ""
-
-    if fields is not None:
-        fields_param = "-f '{fields}'".format(fields=",".join(fields))
-
-    if upsert_fields is not None:
-        upsert_fields_param = "--upsertFields '{upsert_fields}'".format(upsert_fields=",".join(upsert_fields))
-
-    if columns_have_types:
-        columns_have_types_param = "--columnsHaveTypes"
-
-    mongo_import_cmd = """\
-    mongoimport --uri {uri}\
-    -c {collection}\
-    --type {type}\
-    --file {file}\
-    --mode {mode}\
-    {fields_param}\
-    {columns_have_types}\
-    {upsert_fields_param}\
-    {extra_params}\
-    """.format(
-        uri=uri,
-        collection=collection,
-        type=type,
-        file=file,
-        mode=mode,
-        fields_param=fields_param,
-        upsert_fields_param=upsert_fields_param,
-        columns_have_types=columns_have_types_param,
-        extra_params=" ".join(["--{param}".format(param=param) for param in extra_params])
-    )
-
-
-
-    return mongo_import_cmd
-
-
 class S3ToMongoImportOperator(BashOperator):
     @apply_defaults
     def __init__(
@@ -77,7 +20,7 @@ class S3ToMongoImportOperator(BashOperator):
             mongo_upsert_fields=None,
             mongo_import_type='json',
             mongo_columns_have_types=False,
-            mongo_extra_params=[],
+            mongo_extra_params=None,
             xcom_push=False,
             env=None,
             output_encoding='utf-8',
@@ -86,16 +29,15 @@ class S3ToMongoImportOperator(BashOperator):
         mongo_uri = MongoHook(mongo_conn_id).get_uri()
         super(BashOperator, self).__init__(*args, **kwargs)
         self.tmp_file = NamedTemporaryFile()
-        self.bash_command = make_mongo_import_command(
-            uri=mongo_uri,
-            collection=mongo_collection,
-            fields=mongo_fields,
-            upsert_fields=mongo_upsert_fields,
-            type=mongo_import_type,
-            columns_have_types=mongo_columns_have_types,
-            file=self.tmp_file.name,
-            extra_params=mongo_extra_params
-        )
+        self.mongo_uri = mongo_uri
+        self.mongo_collection = mongo_collection
+        self.mongo_fields = mongo_fields
+        self.mongo_upsert_fields = mongo_upsert_fields
+        self.mongo_import_type = mongo_import_type
+        self.mongo_columns_have_types = mongo_columns_have_types
+        self.mongo_extra_params = mongo_extra_params or []
+
+        self.bash_command = self._make_mongo_import_command()
         self.env = env
         self.xcom_push_flag = xcom_push
         self.output_encoding = output_encoding
@@ -103,6 +45,51 @@ class S3ToMongoImportOperator(BashOperator):
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
         self.s3_conn_id = s3_conn_id
+
+    def _make_mongo_import_command(self):
+        """
+        :param query:
+        :param fields:
+        :return:
+        """
+        fields_param = ""
+        upsert_fields_param = ""
+        columns_have_types_param = ""
+
+        if self.mongo_fields is not None:
+            fields_param = "-f '{fields}'".format(fields=",".join(self.mongo_fields))
+
+        if self.mongo_upsert_fields is not None:
+            upsert_fields_param = "--upsertFields '{upsert_fields}'".format(
+                upsert_fields=",".join(self.mongo_upsert_fields))
+
+        if self.mongo_columns_have_types:
+            columns_have_types_param = "--columnsHaveTypes"
+
+        mongo_import_cmd = """\
+        mongoimport --uri {uri}\
+        -c {collection}\
+        --type {type}\
+        --file {file}\
+        --mode {mode}\
+        {fields_param}\
+        {columns_have_types}\
+        {upsert_fields_param}\
+        {extra_params}\
+        """.format(
+            uri=self.mongo_uri,
+            collection=self.mongo_collection,
+            type=self.mongo_type,
+            file=self.mongo_file,
+            mode=self.mongo_mode,
+            fields_param=fields_param,
+            upsert_fields_param=upsert_fields_param,
+            columns_have_types=columns_have_types_param,
+            extra_params=" ".join(
+                ["--{param}".format(param=param) for param in self.mongo_extra_params])
+        )
+
+        return mongo_import_cmd
 
     def execute(self, context):
         self.log.info("downloading file {s3_key} from {s3_bucket}"
